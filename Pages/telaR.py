6c3f7ad7cca9445fdaf5fe8fca71c7b1c94b1a15
Romario Santos <sromario577@gmail.com>
2024-04-20 22:50:12 -0300

    entendendo os dados PagR

--- Pages/telaR.py
+++ Pages/telaR.py
@@ -1,47 +1,72 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-def telaR():
-    st.title('Tela Inicial de Romario')
+# função para Carregar o arquivo CSV em um dataframe pandas
+def lerDataset():
+    dataset = pd.read_csv("heart_disease.csv")
+    return dataset
 
+# função para Contar quantidade de linhas x colunas do dataset
+def contarDataset(dataset):
+    return (f'linhas e colunas: {dataset.shape}')
 
-# Função para carregar os dados
-def carregar_dados(caminho_dados):
-    dados = pd.read_csv(caminho_dados)
-    return dados
+#  função para Visualizar colunas do dataset
+def ColunasDataset(dataset):
+    return dataset.columns
 
+# função para conta quantidade de vezes que um elemento aparece na coluna
+def RepetDataset(dataset, coluna):
+    return dataset[coluna].value_counts()
 
-def main():
-    telaR()
+# função para ler quantidade de entradas unicas numericas
+def entradaNumber(datset):
+    # selecionar colunas numericas
+    numeral_colunas = datset.select_dtypes(include='number')
+
+    # calcular o numero de valores unicos em cada coluna
+    unique_values = numeral_colunas.nunique().sort_values()
+
+    # Cria um gráfico de barras com os valores únicos por coluna
+    unique_values.plot.bar(logy=True, figsize=(15, 4), title="Unique values per feature")
     
-    st.title('Analise exploratoria dos dados com streamilit')
+    # Configurações adicionais do gráfico
+    plt.xlabel("Feature")
+    plt.ylabel("Unique Values")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    
+    # Exibe o gráfico
+    plt.show()
 
-    # carregar os dados
-    caminho_dados = 'heart_disease.csv'
-    dados = carregar_dados(caminho_dados)
+# Interface stremalit
+def main():
+    # Titulo da tela
+    st.title(' Análise exploratória ')
 
-    # Visualização das primeiras linhas do DataFrame
-    st.subheader('visualização das primeiras linhas do dataframe:')
-    st.write(dados.head())
+    # chamar minha função ler dataset
+    dataset = lerDataset()
 
-    # resumo estatistico
-    st.subheader('Resumo estatistico do dataframe:')
-    st.write(dados.describe())
+    # se o dataset nao estiver vazio
+    if lerDataset is not  None:
 
-    # histograma
-    st.subheader('Histograma:')
-    colunas_numericas = dados.select_dtypes(include='number').columns
-    for coluna in colunas_numericas:
-        plt.hist(dados[coluna])
-        st.pyplot()
+        # contar quantidade de linhas x colunas
+        st.write(contarDataset(dataset))
 
-    # matriz de correlaçao
-    st.subheader('Matriz de correlação:')
-    matriz_correlação = dados.corr()
-    sns.heatmap(matriz_correlação, annot=True)
-    st.pyplot()
+        # ler colunas do dataset
+        st.write(ColunasDataset(dataset))
 
-if __name__ == "__main__":
+        # selecionar coluna para conta
+        coluna_selecioanda = st.selectbox('Escolha uma coluna', dataset.columns)
+
+        # contar repetição na coluna selecionada
+        st.write(f'a frequencia da coluna selecionada é {RepetDataset(dataset,coluna_selecioanda)}')
+
+
+        # numero de entradas unicas para cada caracteristica numerica
+        entradaNumber(dataset)
+
+
+# executar def main
+if __name__ == '__main__':
     main()
