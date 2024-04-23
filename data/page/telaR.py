ca80260b3193d9caec17b9d605ab9abf60499f12
Romario Santos <sromario577@gmail.com>
2024-04-23 19:54:31 -0300

    análise dados

--- data/page/telaR.py
+++ data/page/telaR.py
@@ -2,6 +2,21 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
+
+# Função para exibir o boxplot da coluna selecionada
+def exibir_boxplot(dataset, coluna_selecionada):
+    # Verificar se a coluna selecionada existe no dataset
+    if coluna_selecionada in dataset.columns:
+        # Criar o gráfico de boxplot para a coluna selecionada
+        fig, ax = plt.subplots(figsize=(12, 6))
+        dataset[coluna_selecionada].plot(kind='box', ax=ax)
+        ax.set_ylabel('Valor')
+        ax.set_title(f'Boxplot da coluna "{coluna_selecionada}"')
+        plt.tight_layout()
+        st.pyplot(fig)
+    else:
+        st.error('A coluna selecionada não existe no dataset.')
+
 # função para Carregar o arquivo CSV em um dataframe pandas
 def lerDataset():
     dataset = pd.read_csv("data/heart_disease.csv")
@@ -9,7 +24,7 @@ def lerDataset():
 
 # função para Contar quantidade de linhas x colunas do dataset
 def contarDataset(dataset):
-    return (f'linhas e colunas: {dataset.shape}')
+    return dataset.shape
 
 #  função para Visualizar colunas do dataset
 def ColunasDataset(dataset):
@@ -32,6 +47,14 @@ def ConteudoColuna(dataset,coluna):
     
     # Juntar as strings formatadas em uma única string separada por quebras de linha
     return '\n'.join(formatado)
+
+# olhar cinco prmeiras linhas
+def primeiras(dataset):
+    return dataset.head()
+
+# olhar cinco ultimas linhas
+def ultimas(dataset):
+    return dataset.tail()
 
 
 
@@ -73,18 +96,23 @@ def main():
     if lerDataset is not  None:
 
         # contar quantidade de linhas x colunas
+        st.write('Linha e colunas do dataset')
         st.write(contarDataset(dataset))
 
         # ler colunas do dataset
+        st.write('Todas colunas do dataset')
         st.write(ColunasDataset(dataset))
 
         # selecionar coluna para conta
+        st.write('Visualizar uma coluna:')
         coluna_selecionada = st.selectbox('Escolha uma coluna', dataset.columns)
 
         # contar repetição na coluna selecionada
-        st.write(f'a frequencia da coluna selecionada é {RepetDataset(dataset,coluna_selecionada)}')
+        st.write(f'a frequencia da coluna selecionada é:') 
+        st.write(f' \n {RepetDataset(dataset,coluna_selecionada)} ')
         # ler conteudo da coluna
-        st.write(f'conteudo da coluna selecionada: \n{ConteudoColuna(dataset,coluna_selecionada)}')
+        st.write('conteudo da coluna selecionada:')
+        st.write(f'\n {ConteudoColuna(dataset,coluna_selecionada)}')
 
         # dicionario de termos tecnicos do dataset
         st.title(f'Dicionario informativo')
@@ -93,10 +121,34 @@ def main():
         dicionario = ler_arquivo_txt()
         st.write(dicionario)
 
+    
+        st.write('As cinco primeiras linhas:')
+        st.write(primeiras(dataset))
+
+        st.write('As cinco últimas linhas:')
+        st.write(ultimas(dataset))
+
+        st.write('verificar existencia de valores nulos ')
+        st.write(dataset.isnull().sum())
+
+        st.write('verificar tipo de dados ')
+        st.write(dataset.dtypes)
+
+        st.write('Informações estatísticas')
+        st.write(dataset.describe())
+
+        st.write('Entendendo melhor as informações com plot')
+        # Entendendo melhor as informações com plot
+        if st.button('Exibir Boxplot'):
+            exibir_boxplot(dataset, coluna_selecionada)
+
+        st.write('Filtrar dados aparti de um intervalo')
+        st.write(dataset[dataset[coluna_selecionada] >= 40]) # exemplo para coluna BMI
+
+
         # GRAFICO
         # numero de entradas unicas para cada caracteristica numerica 
         entradaNumber(dataset)
-
 
 # executar def main
 if __name__== '__main__':
