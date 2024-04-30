f4551186abc347f70440c7f7510eff32a19c96ce
Julio Vitor <juliovitor99@outlook.com>
2024-04-30 19:20:42 -0300

    adition data profile

--- Paginas/page/telaJ.py
+++ Paginas/page/telaJ.py
@@ -1,5 +1,21 @@
 import streamlit as st
-
+import pandas as pd
+df = pd.read_csv('data/heart_disease.csv')
 def telaJ():
+    
     st.title('Tela Inicial de Julio')
-    st.write('teste')
+    
+
+    st.dataframe(df.head())
+    
+    
+    st.title("Data Profilling")
+        
+        # Carregar o conteúdo do arquivo HTML
+    with open("data/eda.html", "r") as file:
+        pagina_html = file.read()
+
+        # Exibir o conteúdo HTML
+    st.components.v1.html(pagina_html, height = 700, scrolling=True)
+
+    
