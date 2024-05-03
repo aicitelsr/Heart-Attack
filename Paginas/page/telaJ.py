ea4478377ff416e77b10bacad4224409ce71f63e
Julio Vitor <juliovitor99@outlook.com>
2024-05-03 14:59:20 -0300

    melhorias

--- Paginas/page/telaJ.py
+++ Paginas/page/telaJ.py
@@ -5,22 +5,24 @@ df = pd.read_csv('data/heart_disease.csv')
 dfp = pd.read_parquet('data/heart_disease.parquet')
 def telaJ():
     
-    st.title('Tela Inicial de Julio')
+    st.title('Profilling de Dados e Plotagem de Gráficos')
     
 
-    st.dataframe(df.head())
+    st.write('Inserir o Dicionário aqui talvez ??')
     
     
-    st.title("Data Profilling")
-        
+    st.subheader("Data Profilling")
+    
+    with st.expander('Proffiling De Dados'):
+
         # Carregar o conteúdo do arquivo HTML
-    with open("data/eda.html", "r") as file:
-        pagina_html = file.read()
+        with open("data/eda.html", "r") as file:
+            pagina_html = file.read()
 
-        # Exibir o conteúdo HTML
-    st.components.v1.html(pagina_html, height = 700, scrolling=True)
+            # Exibir o conteúdo HTML
+        st.components.v1.html(pagina_html, height = 700, scrolling=True)
 
-    st.title('Categorias Paralelas')
+    st.subheader('Categorias Paralelas')
     nomes_colunas=['HeartDiseaseorAttack','HighBP','HighChol','CholCheck','BMI','Smoker','Stroke','Diabetes','PhysActivity','Fruits','Veggies','HvyAlcoholConsump',
                        'AnyHealthcare','NoDocbcCost','GenHlth','MentHlth','PhysHlth','DiffWalk','Sex','Income','Age','Education']
     colunas= st.multiselect('Colunas',options=nomes_colunas)
@@ -31,5 +33,5 @@ def telaJ():
             grafico= px.parallel_categories(dfp[colunas])
             button_input= st.button('Gerar Gráfico')
             if button_input:
-                st.plotly_chart(grafico)
+                    st.plotly_chart(grafico)
     
