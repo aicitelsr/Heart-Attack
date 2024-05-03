a402dce1e6d97c4abe9975509026e8dab58f163d
Julio Vitor <juliovitor99@outlook.com>
2024-05-03 14:38:52 -0300

    grafico de categorias paralelas/ arquivo parquet inserido

--- Paginas/page/telaJ.py
+++ Paginas/page/telaJ.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 df = pd.read_csv('data/heart_disease.csv')
+dfp = pd.read_parquet('data/heart_disease.parquet')
 def telaJ():
     
     st.title('Tela Inicial de Julio')
@@ -18,4 +20,16 @@ def telaJ():
         # Exibir o conteúdo HTML
     st.components.v1.html(pagina_html, height = 700, scrolling=True)
 
+    st.title('Categorias Paralelas')
+    nomes_colunas=['HeartDiseaseorAttack','HighBP','HighChol','CholCheck','BMI','Smoker','Stroke','Diabetes','PhysActivity','Fruits','Veggies','HvyAlcoholConsump',
+                       'AnyHealthcare','NoDocbcCost','GenHlth','MentHlth','PhysHlth','DiffWalk','Sex','Income','Age','Education']
+    colunas= st.multiselect('Colunas',options=nomes_colunas)
+    if len(colunas) > 4:
+        st.error('Por Favor Insira ')
+    else:
+        if len(colunas) >=2:
+            grafico= px.parallel_categories(dfp[colunas])
+            button_input= st.button('Gerar Gráfico')
+            if button_input:
+                st.plotly_chart(grafico)
     
