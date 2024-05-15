b6b7fb51b86101244c2690da3c396a975af3dd02
Letícia Rodrigues <leticiab.rodrigues@outlook.com>
2024-05-14 22:03:32 -0300

    modificando a main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title='Tela Inicial', layout="wide")
 import Pages.telaJ as pgj
 import Pages.telaL as pgl
 import Pages.telaM as pgm
 import Pages.telaR as pgr
-import streamlit as st
-st.set_page_config(page_title='Tela Inicial', layout="wide")
+
 
