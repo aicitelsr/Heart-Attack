943369a2adb8a8deb345c235739c677e9fb39087
ROMARIO SANTOS <SROMARIO577@GMAIL.COM>
2024-08-20 19:09:34 -0300

    upgrade

--- classificação/regressaoLogistica/regressao.py
+++ classificação/regressaoLogistica/regressao.py
@@ -3,13 +3,13 @@ import pickle
 
 # dividir em treino e teste
 
-arq6 = open('heart.pkl', 'rb')
+arq5 = open('heart.pkl', 'rb')
 arq1 = open('heart.pkl', 'rb')
 
-previsores3_esc = pickle.load(arq6)
+previsores2_esc = pickle.load(arq5)
 alvo = pickle.load(arq1)
 
-x_treino, x_teste, y_treino, y_teste = train_test_split(previsores3_esc, alvo, test_size=0.3, random_state=0)
+x_treino, x_teste, y_treino, y_teste = train_test_split(previsores2_esc, alvo, test_size=0.3, random_state=0)
 x_treino.shape
 
 # Modelo de regressão
