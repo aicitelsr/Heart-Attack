a27186841401cd4b138e174960ca2ccf8ac86613
Marcelo Antonio <marceloa.mnzs@gmail.com>
2024-08-26 18:03:06 -0300

    Adicionando opção de filtragem de outliers para o boxplot

--- utils.py
+++ utils.py
@@ -10,12 +10,12 @@ def readDataframe_parquet() -> pd.DataFrame:
 
 def transformData(df: pd.DataFrame) -> pd.DataFrame:
     # Transformando a coluna idade
-    __transformAgeData(df)
-    __transformIncomeData(df)
-    __transformEducationData(df)
-    __transformDiabetesData(df)
-    __transformBMI(df)
-    __transformGenHealth(df)
+    _transformAgeData(df)
+    _transformIncomeData(df)
+    _transformEducationData(df)
+    _transformDiabetesData(df)
+    _transformBMI(df)
+    _transformGenHealth(df)
     
     # Gerando os dummies
     df_d = pd.get_dummies(df)
@@ -26,40 +26,40 @@ def transformData(df: pd.DataFrame) -> pd.DataFrame:
 
 def transformData2(df: pd.DataFrame) -> pd.DataFrame:
     # Transformando a coluna idade
-    __transformAgeData(df)
-    __transformIncomeData(df)
-    __transformEducationData(df)
-    __transformDiabetesData(df)
-    __transformBMI(df)
-    __transformGenHealth(df)
+    _transformAgeData(df)
+    _transformIncomeData(df)
+    _transformEducationData(df)
+    _transformDiabetesData(df)
+    _transformBMI(df)
+    _transformGenHealth(df)
     dfp2=df
     return dfp2
 
-def __transformAgeData(df: pd.DataFrame) -> pd.DataFrame:
+def _transformAgeData(df: pd.DataFrame) -> pd.DataFrame:
     age = {1:'18-24',2:'25-29',3:'30-34',4:'35-39',5:'40-44',6:'45-49',7:'50-54',8:'55-59',9:'60-64',10:'65-69',11:'70-74',12:'75-79', 13:'Mais de 80'}
     df['Age']= df['Age'].replace(age)
     return df
 
-def __transformIncomeData(df) -> pd.DataFrame:
+def _transformIncomeData(df) -> pd.DataFrame:
     income = {1: 'Menos de $10000', 2:'$10000-$14000' , 3:'$15000-$19999', 4:'$20000-$24999', 5:'$25000-$34999', 6:'$35000-$49999', 7:'$50000-$74999', 8:'$75000 ou mais'}
     df['Income']= df['Income'].replace(income)
     return df
 
-def __transformEducationData(df) -> pd.DataFrame:
+def _transformEducationData(df) -> pd.DataFrame:
     education = {1:'Nunca foi a escola (ou apenas foi à pré-escola)', 2:'Grades 1-8', 3:'Grades 9-11',4:'Grades 12 ou GED', 5:'College 1-3', 6:'College 4 ou mais'}
     df['Education'] = df['Education'].replace(education)
     return df
 
-def __transformDiabetesData(df) -> pd.DataFrame:
+def _transformDiabetesData(df) -> pd.DataFrame:
     diabetes = {0: 'Não possui diabetes', 1:'Pré-diabético', 2:'Diabético'}
     df['Diabetes'] = df['Diabetes'].replace(diabetes)
     return df
 
-def __transformBMI(df) -> pd.DataFrame:
-    df['BMI'] = df['BMI'].apply(__categorizeBMI)
+def _transformBMI(df) -> pd.DataFrame:
+    df['BMI'] = df['BMI'].apply(_categorizeBMI)
     return df
 
-def __categorizeBMI(value):
+def _categorizeBMI(value):
     if value < 16:
         return 'Abaixo do peso'
     elif 16 <= value < 18.5:
@@ -75,6 +75,19 @@ def __categorizeBMI(value):
     else:
         return 'Obesidade Grau III'
     
-def __transformGenHealth(df) -> pd.DataFrame:
+def _transformGenHealth(df) -> pd.DataFrame:
     genHealth = {1:'Execelente', 2:'Boa', 3:'Moderada', 4:'Ruim', 5:'Pobre'}
     df['GenHlth'] = df['GenHlth'].replace(genHealth)  
+
+def removeOutliersFromDf(df) -> pd.DataFrame:
+    Q1 = df['BMI'].quantile(0.25)
+    Q3 = df['BMI'].quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Definir limites para detectar outliers
+    lower_bound = Q1 - 1.0 * IQR
+    upper_bound = Q3 + 1.0 * IQR
+
+# Remover outliers
+    df_cleaned = df[~((df['BMI'] < lower_bound) | (df['BMI'] > upper_bound))]
+    return df_cleaned
