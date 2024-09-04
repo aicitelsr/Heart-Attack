aca4b34fd5fc37b0cb004d16124ead1b937a0b47
Marcelo Antonio <marceloa.mnzs@gmail.com>
2024-09-04 13:59:03 -0300

    Adicionando matrizes de Confusão

--- Pages/4_✅_Matrizes de Confusão.py
+++ Pages/4_✅_Matrizes de Confusão.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.title('Matrizes de Confusão')
+
+with st.expander('Matriz de Confusão - Random Forest'):
+    st.image('./data/matriz_confusao_rf.png')
+with st.expander('Matriz de Confusão - CatBoost'):
+    st.image('./data/matriz_confusao_catboost.png')
+with st.expander('Matriz de Confusão - Regressão Logística'):
+    st.image('./data/matriz_confusao_regressao.png', caption='Matriz de Confusão - Regressão Logística', use_column_width=True)
